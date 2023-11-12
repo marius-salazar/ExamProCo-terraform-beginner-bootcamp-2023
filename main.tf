6d506f5584370a3609855189d6f40d96d36769f6
marius-salazar <122790695+marius-salazar@users.noreply.github.com>
2023-11-12 16:14:09 +0800

    #23 Bucket should no longer use random provider (#24)

--- main.tf
+++ main.tf
@@ -1,16 +1,8 @@
 
+resource "aws_s3_bucket" "website_bucket" {
 
-resource "random_string" "bucket_name" {
-  lower            = true
-  upper            = false
-  length           = 32
-  special          = false
-}
-
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.id
+  bucket = var.bucket_name
+  
 tags = {
 
     UserUuid = var.user_uuid
--- providers.tf
+++ providers.tf
@@ -19,10 +19,7 @@ terraform {
 
 
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+    
 
     aws = {
       source = "hashicorp/aws"
