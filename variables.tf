aab739124fcaa28c075e04730613d1eccf32b339
marius salazar <mariusfranzsalazar@gmail.com>
2023-12-03 04:35:44 +0000

    #29 Updated my TERRAFORM CLOUD TOKEN. Destroyed my resources.

--- variables.tf
+++ variables.tf
@@ -8,11 +8,11 @@ variable "bucket_name" {
 }
 
 
-variable "index_html_filepath" {
-  type = string
-}
-
-
-variable "error_html_filepath" {
-  type = string
-}
+#variable "index_html_filepath" {
+#  type = string
+#}
+#
+#
+#variable "error_html_filepath" {
+#  type = string
+#}
